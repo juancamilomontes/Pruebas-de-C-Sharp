45b108d3c96be36737df4e3e83cb3cd9711d7673
juancamilomontes <jm3876602@gmail.com>
2026-02-14 10:25:22 -0500

    Trabajo de tipos de datos completado

---
+++ Tipos-de-datos/Program.cs
@@ -0,0 +1,19 @@
+﻿namespace ConsoleApp1;
+
+class Program
+{
+    static void Main(string[] args)
+    {
+        int Age = 25;
+        decimal height = 1.75m;
+        Boolean is_student = true;
+        string name = "Ana";
+        char grade = 'A';
+
+        string mensaje = $"Hola {name}, en este momento tienes {Age} años y mides {height}, ¿que si ya eres estudiante? {is_student}, y estas en grado {grade}, muchas felicitaciones.";
+
+        Console.WriteLine(mensaje);
+
+
+    }
+}
